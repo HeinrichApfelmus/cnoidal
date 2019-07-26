0669872c57f846f4f4076aca96c971c64737d02d
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2019-07-26 14:13:38 +0200

    Improve timeline display for `Media`
    Rows are now labeled with their value and sorted.

--- src/Cnoidal/Display.hs
+++ src/Cnoidal/Display.hs
@@ -1,37 +1,51 @@
-{-# LANGUAGE BangPatterns, TypeFamilies #-}
+{-# LANGUAGE BangPatterns, TypeFamilies, FlexibleContexts #-}
 module Cnoidal.Display where
 
+import qualified Data.List
 import qualified Data.Map         as Map
 
 import Diagrams.Prelude
 import Diagrams.Backend.SVG
-import Graphics.Svg               as SVG
+import qualified Graphics.Svg     as SVG
 
 import Hyper
-import Hyper.Extra
+import qualified Data.Text        as T
+import qualified Data.Text.Lazy   as TL
 
 import Cnoidal.Media
+
+type Dia = QDiagram SVG V2 Double Any
+
+{-----------------------------------------------------------------------------
+    Integrate `diagrams-svg` and `svg-builder` wiht HyperHaskell
+------------------------------------------------------------------------------}
+dia :: Dia -> Graphic
+dia = html . TL.toStrict . SVG.renderText
+    . renderDia SVG (SVGOptions absolute Nothing (T.pack "") [] True)
+    -- (mkWidth 250)
 
 {-----------------------------------------------------------------------------
     Display
 ------------------------------------------------------------------------------}
 -- | Display 'Media' as a rectangular boxes on a timeline.
-timeline :: Ord a => Media a -> Graphic
-timeline m = dia $ rectangles $ zip is xs
-    where
-    (is, as) = unzip $ toIntervals m
-    xs       = uniques as
+timeline :: (Show a, Ord a) => Media a -> Graphic
+timeline = dia . pad 1.1 . translateX (-10) . scale 24
+     . vcat . map drawRow . groupRows
 
-rectangles :: [(Interval, Integer)] -> QDiagram SVG V2 Double Any
-rectangles = position . map (uncurry toDia)
+groupRows :: Ord a => Media a -> [(a, [Interval])]
+groupRows media = [(a, map fst $ Prelude.filter ((== a) . snd) ias) | a <- as]
     where
-    height = 0.1 :: Double
-    toDia (t1, mt2) y =
-        ( p2   (a + (b-a)/2, (height * fromIntegral (y-1)))
-        , rect (b-a) height # lw 2)
+    ias = toIntervals media
+    as  = reverse $ Data.List.nub $ Data.List.sort $ map snd ias
+
+drawRow :: Show a => (a, [Interval]) -> Dia
+drawRow (a,xs) = lw 1 $ position $
+    (p2 (-3/2, 0), (text (show a) # scale 0.8) `atop` rect 3 1) : map toRect xs
+    where
+    toRect (t1, mt2) = (p2 (a + (b-a)/2, 0), rect (b-a) 1)
         where
-        a = fromRational t1
-        b = maybe 20 fromRational mt2
+        a = 8*fromRational t1
+        b = 8*maybe 20 fromRational mt2
 
 {-----------------------------------------------------------------------------
     Utilities
